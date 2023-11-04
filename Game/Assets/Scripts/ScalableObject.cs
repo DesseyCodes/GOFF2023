46f7eccf8e2bbe5a6d9c2aff701b5088aa57ac87
DesseyCodes <76536028+DesseyCodes@users.noreply.github.com>
2023-11-04 10:08:54 -0400

    Mouse Position

--- Game/Assets/Scripts/ScalableObject.cs
+++ Game/Assets/Scripts/ScalableObject.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class ScalableObject : MonoBehaviour
 {
+    private Controls controls;
     private void Awake()
     {
-        Controls controls = new Controls();
+        controls = new Controls();
         controls.Mouse.Enable();
 
         controls.Mouse.Hover.performed += (UnityEngine.InputSystem.InputAction.CallbackContext context) =>
@@ -38,7 +39,10 @@ public class ScalableObject : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        //if (controls.Mouse.ShrinkObject.WasPerformedThisFrame)
+        //{
+
+        //}
     }
 
     private void CheckIsHoverInThis(Vector2 hoverPosition)
